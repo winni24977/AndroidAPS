47396d9a0cfcb38fbee8de29c5b625e667ef5c48
winni24977 <winni24977@gmail.com>
2026-09-03 23:00:29 +0200

    Update Versions.kt
    change appVersion from 3.4.2.6dev… to 3.4.2.6

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 object Versions {
 
     // On change edit aaps-ci.yml
-    const val appVersion = "3.4.2.6dev+aisf3.2.1"
+    const val appVersion = "3.4.2.6+aisf3.2.1"
     const val versionCode = 1500
 
     const val compileSdk = 36
